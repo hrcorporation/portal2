81c33bfffafe8769c30c892a7deefdc44ba24dac
hrcorporation <hr.ramirezoli@gmail.com>
2022-01-21 10:57:55 -0500

    ok

--- modules/modulos.php
+++ modules/modulos.php
@@ -8,7 +8,7 @@ if (is_array($array_rol_user =  $login->get_rol_tercero($_SESSION['id_usuario'])
 
 <!--- ============================MODULO USUARIOS CLIENTES============================================= -->
 <?php
-    $modulos = array(2,26); // Array de roles para habilitar roles
+    $modulos = array(1,26); // Array de roles para habilitar roles
     if ($login->validar_rol_user($modulos, $array_rol_user)) : // Validacion para habilitar el usuario
     ?>
 <!-- Modulo HTMl -->
@@ -189,8 +189,69 @@ if (is_array($array_rol_user =  $login->get_rol_tercero($_SESSION['id_usuario'])
     endif; // Fin de la Validacion habilitar modulo
     ?>
 <!--- ============================ FIN MODULO REMIWEBS============================================= -->
+<!--- ============================MODULO USUARIOS CLIENTES============================================= -->
+<?php
+    $modulos = array(1); // Array de roles para habilitar roles
+    if ($login->validar_rol_user($modulos, $array_rol_user)) : // Validacion para habilitar el usuario
+    ?>
+<!-- Modulo HTMl -->
+<div class="col-4" id="">
+    <div class="small-box bg-info ">
+        <div class="ribbon-wrapper ribbon-lg">
+            <div class="ribbon bg-warning">
+                proximamente
+            </div>
+        </div>
+        <div class="inner">
+            <!-- Nombre de Modulo -->
+            <h3>COMPRAS</H3>
+        </div>
+        <div class="icon">
+            <!-- icono del Modulo -->
+            <i class="fas fa-wallet"></i>
+        </div>
+        <!-- Enlace de redireccionamiento del Modulo  -->
+        <a class="small-box-footer disabled" href="compras/">
+            Ir <i class="fas fa-arrow-circle-right"></i>
+        </a>
+    </div>
+</div>
+<?php
+    endif; // Fin de la Validacion habilitar modulo
+    ?>
+<!--- ============================ FIN MODULO REMIWEBS============================================= -->
 
-
+<!--- ============================MODULO USUARIOS CLIENTES============================================= -->
+<?php
+    $modulos = array(1); // Array de roles para habilitar roles
+    if ($login->validar_rol_user($modulos, $array_rol_user)) : // Validacion para habilitar el usuario
+    ?>
+<!-- Modulo HTMl -->
+<div class="col-4" id="">
+    <div class="small-box bg-info ">
+        <div class="ribbon-wrapper ribbon-lg">
+            <div class="ribbon bg-warning">
+                proximamente
+            </div>
+        </div>
+        <div class="inner">
+            <!-- Nombre de Modulo -->
+            <h3>SST</H3>
+        </div>
+        <div class="icon">
+            <!-- icono del Modulo -->
+            <i class="fas fa-wallet"></i>
+        </div>
+        <!-- Enlace de redireccionamiento del Modulo  -->
+        <a class="small-box-footer disabled" href="usuarios_clientes/">
+            Ir <i class="fas fa-arrow-circle-right"></i>
+        </a>
+    </div>
+</div>
+<?php
+    endif; // Fin de la Validacion habilitar modulo
+    ?>
+<!--- ============================ FIN MODULO REMIWEBS============================================= -->
 
 
 <?php
